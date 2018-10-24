542b9f50c8710e0d98e18424a4fd322c7effd6ce
BERNARDO ALTAMIRANO GARCIA <baltamir@itam.mx>
2018-10-24 09:57:00 -0500

    1

---
+++ Parcial2/Parcial2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Parcial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Clave Pedido:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 119);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(482, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(61, 309);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "cambiar fecha";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Parcial2/Parcial2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private GestorBD.GestorBD GestorBDAcc;
+        string cadSql;
+        DataSet dsPedido = new DataSet();
+        DataSet dsPedidoLibro = new DataSet();
+        Varios.Comunes comunes = new Varios.Comunes();
+        const int OK = 1;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //1- Crea el objeto para acceder a la base de datos.
+            GestorBDAcc = new GestorBD.GestorBD("Microsoft.ACE.OLEDB.12.0", "Admin", "",
+                          "C:/Users/BALTAMIRG/ExamenViejo/BD2oPedLibros.accdb");
+
+            //2- Obtiene datos de las materias.
+            cadSql = "select distinct idPed from Pedido";                         //Consulta.
+            GestorBDAcc.consBD(cadSql, dsPedido, "Pedido");           //Se ejecuta.
+            comunes.cargaCombo(comboBox1, dsPedido, "Pedido", "idPed");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //2- Obtiene datos de las materias.
+            cadSql = "select * from PedidoLibro where idPed="+comboBox1.SelectedItem.ToString();                         //Consulta.
+            GestorBDAcc.consBD(cadSql, dsPedidoLibro, "PedidoLibro");           //Se ejecuta.
+            dataGridView1.DataSource = dsPedidoLibro.Tables["PedidoLibro"];        //Muestra resultados.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int folio;
+            DialogResult botón;
+            String fecha;
+
+            //Toma el folio de la calificación seleccionada en el data grid.
+            folio = Convert.ToInt16(dataGridView1["idPed", dataGridView1.CurrentRow.Index].Value);
+            botón = MessageBox.Show("¿Modificaa el registro con idPed = " + folio.ToString(),
+              "Eliminación", MessageBoxButtons.YesNo);
+            //Construye la fecha para el formato de Oracle.
+            fecha = "date'" + dateTimePicker1.Value.Year + "-" + dateTimePicker1.Value.Month + "-" +
+              dateTimePicker1.Value.Day + "'";
+
+            //Si se selecciona el botón Yes, del MessageBox, modifica el registro asociado.
+            if (botón == DialogResult.Yes)
+            {
+                //Construye la cadena de mod y la envía para su ejecución.
+                cadSql = "update PedidoLibro set fechaPed = "+fecha+" where idPed = " + folio;
+                if (GestorBDAcc.cambiaBD(cadSql) == OK)
+                    MessageBox.Show("Se modifico el registro del folio " + folio + " exitosamente");
+                else
+                    MessageBox.Show("No se pudo modificar");
+            }
+        }
+    }
+}
